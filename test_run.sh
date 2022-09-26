d0fb00fdca7d06b1a3f5fac1a2307a1abe318179
Barney Hill <barneyhill1@gmail.com>
2022-09-26 17:04:04 +0100

    Added printing to test run

--- test_run.sh
+++ test_run.sh
@@ -5,5 +5,4 @@ Rscript RV_meta.R \
 --info_file_path test_input/cohort1/LD_mat/cohort1_chr_7.marker_info.txt test_input/cohort2/LD_mat/cohort2_chr_7.marker_info.txt \
 --gene_file_prefix test_input/cohort1/LD_mat/cohort1_chr_7_ test_input/cohort2/LD_mat/cohort2_chr_7_ \
 --gwas_path test_input/cohort1/GWAS_summary/t2d_cohort1_step2_res_7.txt test_input/cohort2/GWAS_summary/t2d_cohort2_step2_res_7.txt \
---output_prefix test_output/t2d_chr7_0.01_missense_lof_res.txt \
-> test_log.out
+--output_prefix test_output/t2d_chr7_0.01_missense_lof_res.txt | tee test_log.out
